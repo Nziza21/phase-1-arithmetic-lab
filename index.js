8aea134523ff6d4fd50b10968ebc6d74f22536b9
Nziza Samuel <nziza.samuel@student.moringaschool.com>
2023-09-20 22:21:49 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
+const num1 = 31;
+const num2 = 2;
 
-// Write your code here
+const multiply = num1 * num2;
+
+console.log(multiply);
+
+
+
+const random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+
+console.log(random);
+
+
+const num3 = 17; 
+const num4 = 13; 
+
+const mod = num3 % num4;
+
+console.log(mod);
+
+
+const numbers = [10, 5, 20, 15, 7]; // Replace this array with your set of numbers
+
+const max = Math.max(...numbers);
+
+console.log(max);
